dc6dfa85b02c9866221c2e0b27f7c7c81a1d1874
diana <gausvsvs6@gmail.com>
2026-07-21 05:45:47 +0700

    Laravel

---
+++ Laravel/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return "Hi! Selamat Datang di Website Laravel";
+});
+
+Route::get('/hello', function () {
+    return "Hello World";
+});
+
+Route::get('/world', [WelcomeController::class, 'hello']);
+
+Route::get('/siswa', function () {});
+Route::post('/siswa', function () {});
+Route::put('/siswa', function () {});
+Route::delete('/siswa', function () {});
+
+Route::get('/siswa/{id}', function ($id) {});
+Route::put('/siswa/{id}', function ($id) {});
+Route::delete('/siswa/{id}', function ($id) {});
+
+Route::get('/a', function (){
+    echo "Hi! Selamat datang di Website Laravel";
+});
+Route::get('/about', function () {
+    echo "NIM: 25982<br>";
+    echo "Nama: Diana Pramudita<br>";
+    echo "Kelas: XI-RPB<br><br>";
+
+    echo "NIM: 26006<br>";
+    echo "Nama: Mutia Izza M.<br>";
+    echo "Kelas: XI-RPB";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Ini adalah halaman Artikel dengan ID: " . $id;
+});
